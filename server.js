518dae3e698b6e84ff101c592f7655b97321cf9f
Samusoidal <info@samusoidal.com>
2018-07-07 20:49:42 -0700

    main

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+
 // Middleware
 server.use(restifyPlugins.jsonBodyParser({ mapParams: true }));
 server.use(restifyPlugins.acceptParser(server.acceptable));
@@ -30,9 +50,11 @@ server.use(restifyPlugins.authorizationParser());
 
 // Start Server, Connect to DB and Require Routes
 server.listen(port, () => {
+
   //connect to mongodb
   mongoose.Promise = global.Promise;
-  mongoose.connect(mongoURL, { useMongoClient: true }, function(err, conn) {
+
+  mongoose.connect(mongoURL, function(err, conn) {
     if (err) {
       callback(err);
       return;
